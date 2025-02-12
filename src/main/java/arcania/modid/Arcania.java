d85ce1922a178b5e533d984abf67cd2214a77ee6
hansib <ensar@ensars-mbp.speedport.ip>
2025-02-12 21:31:46 +0100

    1st commit

---
+++ src/main/java/arcania/modid/Arcania.java
@@ -0,0 +1,16 @@
+package arcania.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Arcania implements ModInitializer {
+	public static final String MOD_ID = "arcania";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/arcania/modid/ArcaniaClient.java
@@ -0,0 +1,10 @@
+package arcania.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaniaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
