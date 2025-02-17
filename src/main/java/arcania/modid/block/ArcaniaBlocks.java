180ea6b98013b3d3009552091043cd2d95653bf8
hansib <ensar@Ensars-MacBook-Pro.local>
2025-02-17 16:05:21 +0100

    Added Ores for Orbs, including loot tables

--- src/main/java/arcania/modid/block/ArcaniaBlocks.java
+++ src/main/java/arcania/modid/block/ArcaniaBlocks.java
@@ -4,6 +4,7 @@ import arcania.modid.Arcania;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,20 +12,20 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ArcaniaBlocks {
 
     public static final Block SPELL_TABLE = registerBlock("spell_table", new Block(AbstractBlock.Settings.create().strength(4f)
-            .requiresTool()
             .sounds(BlockSoundGroup.WOOD)));
 
-    public static final Block RED_ORB_ORE = registerBlock("red_orb_ore", new Block(AbstractBlock.Settings.create().strength(4f)
-            .requiresTool()
-            .sounds(BlockSoundGroup.STONE)));
+    public static final Block RED_ORB_ORE = registerBlock("red_orb_ore", new ExperienceDroppingBlock(
+            UniformIntProvider.create(2, 5),
+            AbstractBlock.Settings.create().strength(3f).requiresTool()));
 
-    public static final Block GREEN_ORB_ORE = registerBlock("green_orb_ore", new Block(AbstractBlock.Settings.create().strength(4f)
-            .requiresTool()
-            .sounds(BlockSoundGroup.STONE)));
+    public static final Block GREEN_ORB_ORE = registerBlock("green_orb_ore", new ExperienceDroppingBlock(
+            UniformIntProvider.create(3, 6),
+            AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
